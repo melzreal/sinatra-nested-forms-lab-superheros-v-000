5f8360483eaff56cadfae94339bf52263ca7c652
Melissa Real <melissa.real@hotmail.com>
2018-11-14 11:35:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-    get '/' do 
-      
-    end 
+    get '/' do
 
-    post '/teams' do 
+    end
 
-    end 
+    post '/teams' do
+
+    end
 
 end
